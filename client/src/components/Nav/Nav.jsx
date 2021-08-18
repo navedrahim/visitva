eccc4cd051255c8ce02d46e43047a28f6412f95b
navedrahim <nrah1988@gmail.com>
2021-08-18 13:00:15 -0400

    some media queries and hamburger menu

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,9 +1,28 @@
 import { NavLink } from "react-router-dom";
 import "./Nav.css"
+import { useEffect, useState } from "react";
+
 const Nav = ({ user }) => {
+  const [visible, setVisible] = useState(false);
+  const [hamburger, setHamburger] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setVisible(true);
+        setHamburger(false);
+      } else {
+        setVisible(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
 const authenticatedOptions = (
   <>
-    {/* <h4>{`Hello, ${user?.username}!`}</h4> */}
     <NavLink className="link" to="/add">
       Add Post
     </NavLink>
@@ -38,6 +57,14 @@ const alwaysOptions = (
         <NavLink className="logo" to="/">
           VisitVA
         </NavLink>
+        <button
+          className="hamburger" id="hamburger-button"
+          onClick={() => setHamburger(!hamburger)}
+        >
+          <div className="hamburger-detail"></div>
+          <div className="hamburger-detail"></div>
+          <div className="hamburger-detail"></div>
+        </button>
         <div className="nav">
           <div className="links">
           <h4>{user ? `Hello, ${user?.username}!`: null}</h4>{alwaysOptions}
@@ -45,6 +72,19 @@ const alwaysOptions = (
           </div>
         </div>
       </nav>
+      <div
+        style={{
+          display:
+            (visible && window.innerWidth < 768) ||
+            (hamburger && window.innerWidth < 768)
+              ? "flex"
+              : "none",
+        }}
+        className="hamburger-links"
+      >
+        <h4>{user ? `Hello, ${user?.username}!`: null}</h4>{alwaysOptions}
+        {user ? authenticatedOptions : unauthenticatedOptions}
+      </div>
     </>
   );
 };
